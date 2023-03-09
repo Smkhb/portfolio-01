e234ec5ec5b2a1f489b4928a0c58c2d810b513fe
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-09 15:13:11 -0300

    O que eu utilizo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BsFillMoonStarsFill } from "react-icons/bs";
-import { AiFillTwitterCircle, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { AiFillTwitterCircle, AiFillGithub, AiFillLinkedin, AiFillHtml5 } from "react-icons/ai";
+import { SiCss3, SiJavascript, SiNodedotjs, SiTailwindcss, SiSass } from "react-icons/si";
+import { DiReact, DiGit } from "react-icons/di";
 import avatar from '../images/smk-avatar.png'
 
 function App() {
@@ -8,9 +10,9 @@ function App() {
   return (
     <div>
       <main className='bg-white px-10'>
-        
+
         <section className=' min-h-screen'>
-          
+
           <nav className='py-10 mb-12 flex justify-between'>
             <h1 className=' text-xl'>Samuel Bernardo</h1>
             <ul className='flex items-center'>
@@ -42,6 +44,107 @@ function App() {
             <img src={avatar} />
           </div>
 
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>O que eu utilizo</h3>
+            <p className='text-md py-2 leading-8 text-gray-800'>Desde que comecei minha jornada como programador essas sao algumas das ferramentas que aprendi a utilizar no meu dia a dia.</p>
+          </div>
+
+          <div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-red-500 '>
+                <AiFillHtml5 />
+              </div>
+              <h3 className='text-lg font-medium py-2'>HTML</h3>
+              <p className='py-2 text-gray-800'>
+                É a base da web moderna e é utilizado para criar todos os tipos de sites e aplicações web, desde páginas estáticas até sistemas complexos de gerenciamento de conteúdo. Sem o HTML, a internet como a conhecemos hoje não seria possível.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-sky-500 '>
+                <SiCss3 />
+              </div>
+              <h3 className='text-lg font-medium py-2'>CSS</h3>
+              <p className='py-2 text-gray-800'>
+                É uma linguagem de estilo que permite que os desenvolvedores controlem a aparência visual de páginas web. Ele é uma parte essencial do desenvolvimento web moderno e é utilizado em conjunto com o HTML e outras tecnologias para criar páginas web que sejam atraentes e funcionais.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-yellow-400 '>
+                <SiJavascript />
+              </div>
+              <h3 className='text-lg font-medium py-2'>Javascript</h3>
+              <p className='py-2 text-gray-800'>
+                É uma linguagem de estilo que permite que os desenvolvedores controlem a aparência visual de páginas web. Ele é uma parte essencial do desenvolvimento web moderno e é utilizado em conjunto com o HTML e outras tecnologias para criar páginas web que sejam atraentes e funcionais.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-green-600 '>
+                <SiNodedotjs />
+              </div>
+              <h3 className='text-lg font-medium py-2'>Node.Js</h3>
+              <p className='py-2 text-gray-800'>
+                É uma plataforma de desenvolvimento em JavaScript que permite criar aplicações web escaláveis e de alto desempenho. Ele é compatível com uma ampla gama de bibliotecas e módulos e tem uma ampla comunidade de desenvolvedores e recursos disponíveis. Ele é usado para a criação de aplicações web completas, ferramentas de linha de comando, aplicativos de desktop e jogos.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-sky-400 '>
+                <DiReact />
+              </div>
+              <h3 className='text-lg font-medium py-2'>React.Js</h3>
+              <p className='py-2 text-gray-800'>
+                É uma biblioteca de desenvolvimento de interfaces de usuário para aplicações web. Ele permite que os desenvolvedores criem componentes reutilizáveis e modulares, atualizem a interface de forma rápida e eficiente, e é compatível com outras tecnologias e bibliotecas. Ele é uma escolha popular para o desenvolvimento de aplicações web modernas e eficientes.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-sky-300 '>
+                <SiTailwindcss />
+              </div>
+              <h3 className='text-lg font-medium py-2'>Tailwind</h3>
+              <p className='py-2 text-gray-800'>
+                É um framework de estilo para a criação de interfaces de usuário em aplicações web. Ele é projetado para tornar a criação de estilos em aplicações web mais eficiente e fácil para os desenvolvedores.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-pink-400 '>
+                <SiSass />
+              </div>
+              <h3 className='text-lg font-medium py-2'>Sass</h3>
+              <p className='py-2 text-gray-800'>
+                O SASS é uma extensão do CSS que permite aos desenvolvedores escrever estilos de forma mais eficiente e organizada. Ele é projetado para tornar a escrita de estilos em aplicações web mais fácil e rápida.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center text-red-400 '>
+                <DiGit />
+              </div>
+              <h3 className='text-lg font-medium py-2'>Git</h3>
+              <p className='py-2 text-gray-800'>
+                É uma ferramenta de controle de versão que ajuda os desenvolvedores de software a gerenciar e controlar as alterações feitas em seus projetos. Essencial para o trabalho colaborativo em projetos de desenvolvimento de software, permitindo que várias pessoas trabalhem juntas sem que haja conflitos e sem perder o histórico de alterações feitas no código.
+              </p>
+            </div>
+
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <div className='text-6xl flex justify-center  '>
+                <AiFillGithub />
+              </div>
+              <h3 className='text-lg font-medium py-2'>GitHub</h3>
+              <p className='py-2 text-gray-800'>
+              O GitHub é uma plataforma online que utiliza o Git como base e oferece uma série de recursos adicionais para desenvolvedores de software, permitindo que eles armazenem, gerenciem, colaborem e compartilhem seus projetos de forma segura e eficiente, além de oferecer recursos adicionais que tornam o processo de desenvolvimento mais fácil e rápido.
+              </p>
+            </div>
+
+          </div>
         </section>
       </main>
     </div>
