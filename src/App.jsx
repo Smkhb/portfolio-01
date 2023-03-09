9ff7f950df6dba251b1a95c28abd12ed13584642
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-09 13:29:26 -0300

    Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
-
+import { BsFillMoonStarsFill } from "react-icons/bs";
 function App() {
 
   return (
     <div>
-      <main>
-        <section>
-          <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <main className='bg-white px-10'>
+        <section className=' min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className=' text-xl'>Samuel Bernardo</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className=' cursor-pointer text-2xl' />
+              </li>
+              <li>
+                <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
+              </li>
+            </ul>
+          </nav>
         </section>
       </main>
     </div>
