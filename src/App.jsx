c1a7a4b5241e95db31fdbc01448eccfe94df659e
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-03-13 19:08:42 -0300

    Revert "💻"
    This reverts commit f12f286005ae0170cda4a4d7e22503c4be89d23b.

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function App() {
           </nav>
 
           <div className='text-center p-10'>
-            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Samuel Bernardo <span className=''>💻</span></h2>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Samuel Bernardo <span className=''>🤚</span></h2>
             <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Desenvolvedor Front-End Jr</h3>
             <p className='text-md py-5 leading-8 text-gray-600 md:text-xl max-w-xl mx-auto dark:text-slate-300'>
               Sou um Desenvolvedor apaixonado pelo meu trabalho, sempre estou
